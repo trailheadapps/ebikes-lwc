4753f3fa0f3660bebe74ef121ee8f28172a5a786
Trevor <tbliss@salesforce.com>
2019-01-02 16:26:52 -0800

    more productTileList tests

--- force-app/main/default/lwc/productTileList/__tests__/productTileList.test.js
+++ force-app/main/default/lwc/productTileList/__tests__/productTileList.test.js
@@ -1,5 +1,6 @@
 import { createElement } from 'lwc';
 import ProductTileList from 'c/productTileList';
+import { fireEvent } from 'c/pubsub';
 import {
     registerTestWireAdapter,
     registerLdsTestWireAdapter,
@@ -18,11 +19,16 @@ jest.mock('c/pubsub', () => {
 
 // TODO(tbliss): the default resolution for @salesforce/apex/foo.bar is a function that returns a resolved Promise.
 //               that errors when it's used as an @wire id
-jest.mock('@salesforce/apex/ProductController.getProducts', () => {
-    return { default: jest.fn() };
-}, { virtual: true });
+jest.mock(
+    '@salesforce/apex/ProductController.getProducts',
+    () => {
+        return { default: jest.fn() };
+    },
+    { virtual: true },
+);
 
-const mockGetProductsValues = require('./data/getProducts.json');
+const mockGetProducts = require('./data/getProducts.json');
+const mockGetProductsNoRecords = require('./data/getProductsNoRecords.json');
 
 // Register as an LDS wire adapter. Some tests verify the provisioned values trigger desired behavior.
 const getProductsAdapter = registerLdsTestWireAdapter(getProducts);
@@ -31,8 +37,7 @@ const getProductsAdapter = registerLdsTestWireAdapter(getProducts);
 // We don't exercise this wire adpater in the tests.
 registerTestWireAdapter(CurrentPageReference);
 
-describe.only('c-product-tile-list', () => {
-
+describe('c-product-tile-list', () => {
     afterEach(() => {
         // The jsdom instance is shared across test cases in a single file so reset the DOM
         while (document.body.firstChild) {
@@ -40,19 +45,161 @@ describe.only('c-product-tile-list', () => {
         }
     });
 
-    describe('c-paginator', () => {
-        it('foo', () => {
+    describe('with records', () => {
+        it('renders paginator with correct item counts', () => {
             const element = createElement('c-product-tile-list', {
                 is: ProductTileList,
             });
-            getProductsAdapter.emit(mockGetProductsValues);
-
             document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProducts);
+
+            return Promise.resolve().then(() => {
+                const paginator = element.shadowRoot.querySelector(
+                    'c-paginator',
+                );
+                expect(paginator).not.toBeNull();
+
+                // we know from the mock data (./data/getProducts.json) we
+                // should have 12 total items and a page size of 9
+                expect(paginator.shadowRoot.textContent).toMatch(
+                    /12 items(.*)page 1 of 2/,
+                );
+            });
+        });
+
+        it('increments/decrements page number when "next" and "previous" events fired', () => {
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProducts);
+            return Promise.resolve()
+                .then(() => {
+                    const paginator = element.shadowRoot.querySelector(
+                        'c-paginator',
+                    );
+                    paginator.dispatchEvent(new CustomEvent('next'));
+                })
+                .then(() => {
+                    // DOM is updated after event is fired so need to wait
+                    // another microtask for the rerender
+                    const paginator = element.shadowRoot.querySelector(
+                        'c-paginator',
+                    );
+                    expect(paginator.shadowRoot.textContent).toMatch(
+                        /page 2 of 2/,
+                    );
+
+                    paginator.dispatchEvent(new CustomEvent('previous'));
+                })
+                .then(() => {
+                    const paginator = element.shadowRoot.querySelector(
+                        'c-paginator',
+                    );
+                    expect(paginator.shadowRoot.textContent).toMatch(
+                        /page 1 of 2/,
+                    );
+                });
+        });
+
+        it('displays one c-product-tile per record', () => {
+            const recordCount = 9;
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProducts);
+            return Promise.resolve().then(() => {
+                const productTiles = element.shadowRoot.querySelectorAll(
+                    'c-product-tile',
+                );
+                expect(productTiles).toHaveLength(recordCount);
+            });
+        });
+
+        it('fires productSelected event when c-product-tile selected', () => {
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProducts);
+            return Promise.resolve().then(() => {
+                const productTile = element.shadowRoot.querySelector(
+                    'c-product-tile',
+                );
+                productTile.dispatchEvent(new CustomEvent('selected'));
+                expect(fireEvent).toHaveBeenCalledWith(
+                    undefined,
+                    'productSelected',
+                    null,
+                );
+            });
+        });
+    });
+
+    describe('without records', () => {
+        it('does not render paginator', () => {
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProductsNoRecords);
 
             const paginator = element.shadowRoot.querySelector('c-paginator');
-            expect(paginator).not.toBeNull();
+            expect(paginator).toBeNull();
+        });
 
-            // expect(element).toMatchSnapshot();
+        it('renders placeholder with no products message', () => {
+            const expected =
+                'There are no products matching your current selection';
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.emit(mockGetProductsNoRecords);
+
+            return Promise.resolve().then(() => {
+                const placeholder = element.shadowRoot.querySelector(
+                    'c-placeholder',
+                );
+                expect(placeholder.shadowRoot.textContent).toBe(expected);
+            });
+        });
+    });
+
+    describe('getProducts @wire error', () => {
+        it('shows error message element', () => {
+            const error = {
+                details: {
+                    body: {
+                        message: 'error from test',
+                    },
+                },
+            };
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            document.body.appendChild(element);
+            getProductsAdapter.error(error);
+            return Promise.resolve().then(() => {
+                const inlineMessage = element.shadowRoot.querySelector(
+                    'c-inline-message',
+                );
+                expect(inlineMessage).not.toBeNull();
+            });
+        });
+    });
+
+    // TODO(tbliss): how to verify search bar? filters is non-API and only connected to wire
+    describe('with search bar visible', () => {
+        it('renders lightning-input as search bar', () => {
+            const element = createElement('c-product-tile-list', {
+                is: ProductTileList,
+            });
+            element.searchBarIsVisible = true;
+            document.body.appendChild(element);
+            const searchBar = element.shadowRoot.querySelector('.search-bar');
+            expect(searchBar.tagName.toLowerCase()).toBe('lightning-input');
         });
     });
 });
