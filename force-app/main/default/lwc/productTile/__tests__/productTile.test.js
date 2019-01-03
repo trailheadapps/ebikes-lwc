c0a0f4d8e0631bc8f00d2c9898e70a6a37422233
Trevor <tbliss@salesforce.com>
2019-01-02 21:38:26 -0800

    productTile tests

---
+++ force-app/main/default/lwc/productTile/__tests__/productTile.test.js
@@ -0,0 +1,59 @@
+import { createElement } from 'lwc';
+import ProductTile from 'c/productTile';
+
+describe('c-product-tile', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('dragging sets product as dataTransfer data', () => {
+        const element = createElement('c-product-tile', {
+            is: ProductTile,
+        });
+
+        // emulate a DragEvent
+        const dragstartEvent = new CustomEvent('dragstart');
+        dragstartEvent.dataTransfer = {
+            setData: jest.fn(),
+        };
+
+        const product = {
+            Id: 1,
+            Picture_URL__c: 'https://example.com',
+            Name: 'Foo',
+            MSRP__c: 1000,
+        };
+        element.product = product;
+        document.body.appendChild(element);
+
+        const div = element.shadowRoot.querySelector('div');
+        div.dispatchEvent(dragstartEvent);
+
+        expect(dragstartEvent.dataTransfer.setData).toHaveBeenCalledWith(
+            'product',
+            JSON.stringify(product),
+        );
+    });
+
+    it('clicking fires selected event', () => {
+        const listener = jest.fn();
+        const element = createElement('c-product-tile', {
+            is: ProductTile,
+        });
+        element.addEventListener('selected', listener);
+        const product = {
+            Id: 1,
+            Picture_URL__c: 'https://example.com',
+            Name: 'Foo',
+            MSRP__c: 1000,
+        };
+        element.product = product;
+        document.body.appendChild(element);
+        const anchor = element.shadowRoot.querySelector('a');
+        anchor.click();
+        expect(listener).toHaveBeenCalled();
+    });
+});
