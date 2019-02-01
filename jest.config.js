ffe2bc54696920adb0584e3ba56f94d7a705e74d
Nathan Totten <ntotten@salesforce.com>
2019-02-01 09:22:15 -0500

    prettier settings

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const { jestConfig } = require('@salesforce/lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
-        '^lightning/navigation$': '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+        '^lightning/navigation$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
     }
 };
