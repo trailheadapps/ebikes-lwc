07ce53e31c476016b755797883b294238d361c74
pozil <philippe.ozil@gmail.com>
2020-02-06 11:09:47 +0100

    Auto publish community

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -11,5 +11,6 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n ebikes
 sfdx force:data:tree:import --plan ./data/sample-data-plan.json
 sfdx force:mdapi:deploy -u ebikes --deploydir mdapiDeploy/unpackaged -w 1
+sfdx force:community:publish -n E-Bikes
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
