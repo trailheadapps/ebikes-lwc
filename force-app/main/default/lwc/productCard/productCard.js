0a23f93be50156124f770aa4f8d76cee56b08c6f
Philippe Ozil <5071767+pozil@users.noreply.github.com>
2020-04-02 14:31:16 +0200

    Converted product card to use view-form (#166)
    * Converted product card to use view-form
    * Discarded sample data csv files
    * Enriched sample products
    * Enforced referential integrity for Product fields

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -2,8 +2,8 @@ import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import { NavigationMixin } from 'lightning/navigation';
 
-/** Wire adapter to load records, utils to extract values. */
-import { getRecord } from 'lightning/uiRecordApi';
+/** Utils to extract field values. */
+import { getFieldValue } from 'lightning/uiRecordApi';
 
 /** Pub-sub mechanism for sibling component communication. */
 import { registerListener, unregisterAllListeners } from 'c/pubsub';
@@ -11,34 +11,42 @@ import { registerListener, unregisterAllListeners } from 'c/pubsub';
 /** Product__c Schema. */
 import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
 import NAME_FIELD from '@salesforce/schema/Product__c.Name';
-import LEVEL_FIELD from '@salesforce/schema/Product__c.Level__c';
-import CATEGORY_FIELD from '@salesforce/schema/Product__c.Category__c';
-import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
-import MSRP_FIELD from '@salesforce/schema/Product__c.MSRP__c';
 import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picture_URL__c';
-
-/** Record fields to load. */
-const fields = [
-    NAME_FIELD,
-    LEVEL_FIELD,
-    CATEGORY_FIELD,
-    MATERIAL_FIELD,
-    MSRP_FIELD,
-    PICTURE_URL_FIELD
-];
+import CATEGORY_FIELD from '@salesforce/schema/Product__c.Category__c';
+import LEVEL_FIELD from '@salesforce/schema/Product__c.Level__c';
+import MSRP_FIELD from '@salesforce/schema/Product__c.MSRP__c';
+import BATTERY_FIELD from '@salesforce/schema/Product__c.Battery__c';
+import CHARGER_FIELD from '@salesforce/schema/Product__c.Charger__c';
+import MOTOR_FIELD from '@salesforce/schema/Product__c.Motor__c';
+import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
+import FOPK_FIELD from '@salesforce/schema/Product__c.Fork__c';
+import FRONT_BRAKES_FIELD from '@salesforce/schema/Product__c.Front_Brakes__c';
+import REAR_BRAKES_FIELD from '@salesforce/schema/Product__c.Rear_Brakes__c';
 
 /**
  * Component to display details of a Product__c.
  */
 export default class ProductCard extends NavigationMixin(LightningElement) {
-    /** Id of Product__c to display. */
+    // Exposing fields to make them available in the template
+    categoryField = CATEGORY_FIELD;
+    levelField = LEVEL_FIELD;
+    msrpField = MSRP_FIELD;
+    batteryField = BATTERY_FIELD;
+    chargerField = CHARGER_FIELD;
+    motorField = MOTOR_FIELD;
+    materialField = MATERIAL_FIELD;
+    forkField = FOPK_FIELD;
+    frontBrakesField = FRONT_BRAKES_FIELD;
+    rearBrakesField = REAR_BRAKES_FIELD;
+
+    // Id of Product__c to display
     recordId;
 
-    @wire(CurrentPageReference) pageRef;
+    // Product fields displayed with specific format
+    productName;
+    productPictureUrl;
 
-    /** Load the Product__c to display. */
-    @wire(getRecord, { recordId: '$recordId', fields })
-    product;
+    @wire(CurrentPageReference) pageRef;
 
     connectedCallback() {
         registerListener('productSelected', this.handleProductSelected, this);
@@ -48,9 +56,16 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
         unregisterAllListeners(this);
     }
 
+    handleRecordLoaded(event) {
+        const { records } = event.detail;
+        const recordData = records[this.recordId];
+        this.productName = getFieldValue(recordData, NAME_FIELD);
+        this.productPictureUrl = getFieldValue(recordData, PICTURE_URL_FIELD);
+    }
+
     /**
-     * Handler for when a product is selected. When `this.recordId` changes, the @wire
-     * above will detect the change and provision new data.
+     * Handler for when a product is selected. When `this.recordId` changes, the
+     * lightning-record-view-form component will detect the change and provision new data.
      */
     handleProductSelected(productId) {
         this.recordId = productId;
@@ -65,9 +80,5 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
                 actionName: 'view'
             }
         });
-    }
-
-    get noData() {
-        return !this.product.error && !this.product.data;
     }
 }
