6c8e7950fe39d4beb896d0c91c605616af746b08
pozil <philippe.ozil@gmail.com>
2020-05-26 11:32:27 +0200

    Removed unneeded ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,5 @@ module.exports = {
     moduleNameMapper: {
         '^lightning/navigation$':
             '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
-    },
-    testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/test/specs/',
-        '<rootDir>/lcc-react-js-bike-model/'
-    ]
+    }
 };
