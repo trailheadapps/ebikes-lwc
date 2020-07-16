0260023547c360ea9b2e4e866ebc1f62a48f0e29
Stephan Chandler-Garcia <stephan@gdprsuperheroes.com>
2020-07-16 11:31:29 -0700

    creation of the heroDeatils LWC test

---
+++ force-app/main/default/lwc/heroDetails/__tests__/heroDetails.test.js
@@ -0,0 +1,78 @@
+import { createElement } from 'lwc';
+import HeroDetails from 'c/heroDetails';
+import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
+import getRecordInfo from '@salesforce/apex/ProductRecordInfoController.getRecordInfo';
+
+// Mock realistic data for the getRecordInfo adapter
+const mockGetRecordInfoProduct = require('./data/getRecordInfoProduct.json');
+const mockGetRecordInfoProductFamily = require('./data/getRecordInfoProductFamily.json');
+
+// Mock realistic data for the public properties
+const mockRecordName = 'Electra';
+
+//Expected Wire Input
+const WIRE_INPUT = {
+    productOrFamilyName: 'Electra'
+};
+
+// Register as an LDS wire adapter. Some tests verify the provisioned values trigger desired behavior.
+const getRecordInfoAdapter = registerApexTestWireAdapter(getRecordInfo);
+
+describe('c-hero-details', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('sets the href URL to a Product__c', () => {
+        const element = createElement('c-hero-details', {
+            is: HeroDetails
+        });
+        element.recordName = mockRecordName;
+        document.body.appendChild(element);
+
+        // Emit Data from the Apex wire adapter.
+        getRecordInfoAdapter.emit(mockGetRecordInfoProduct);
+
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // Check the wire parameters are correct
+            expect(getRecordInfoAdapter.getLastConfig()).toEqual(WIRE_INPUT);
+            // Select elements for validation
+            const anchorEl = element.shadowRoot.querySelector('a');
+            expect(anchorEl).not.toBeNull();
+            expect(anchorEl.href).toBe(
+                `http://localhost/product/${mockGetRecordInfoProduct[0]}`
+            );
+        });
+    });
+
+    it('sets the href URL to a Product_Family__c', () => {
+        const element = createElement('c-hero-details', {
+            is: HeroDetails
+        });
+        element.recordName = mockRecordName;
+        document.body.appendChild(element);
+
+        // Emit Data from the Apex wire adapter.
+        getRecordInfoAdapter.emit(mockGetRecordInfoProductFamily);
+
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // Check the wire parameters are correct
+            expect(getRecordInfoAdapter.getLastConfig()).toEqual(WIRE_INPUT);
+            // Select elements for validation
+            const anchorEl = element.shadowRoot.querySelector('a');
+            expect(anchorEl).not.toBeNull();
+            expect(anchorEl.href).toBe(
+                `http://localhost/product-family/${mockGetRecordInfoProduct[0]}`
+            );
+        });
+    });
+});
--- force-app/main/default/lwc/heroDetails/heroDetails.js
+++ force-app/main/default/lwc/heroDetails/heroDetails.js
@@ -25,31 +25,4 @@ export default class HeroDetails extends NavigationMixin(LightningElement) {
             }
         }
     }
-
-    // Implement this when lightning-navigation in Communities is fixed
-    onButtonClickHandler(evt) {
-        // Stop the event's default behavior.
-        // Stop the event from bubbling up in the DOM.
-        evt.preventDefault();
-        evt.stopPropagation();
-
-        if (
-            !this.recordInfoData ||
-            !this.recordInfoData.data ||
-            this.recordInfoData.error
-        ) {
-            // eslint-disable-next-line no-console
-            console.error('No record information! Cannot navigate anywhere!');
-            return;
-        }
-
-        this[NavigationMixin.Navigate]({
-            type: 'standard__recordPage',
-            attributes: {
-                recordId: this.recordInfoData.data[0],
-                objectApiName: this.recordInfoData.data[1],
-                actionName: 'view'
-            }
-        });
-    }
 }
