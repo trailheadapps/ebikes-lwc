b255b5d77b7ac22645c1d9e451dfa17f480e97de
Stephan Chandler-Garcia <stephan@gdprsuperheroes.com>
2020-07-17 07:59:15 -0700

    update to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
+        '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex',
         '^lightning/navigation$':
             '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
         '^lightning/messageService$':
