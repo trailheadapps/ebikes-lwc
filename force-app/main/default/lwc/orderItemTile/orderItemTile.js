c27758749d90a72e3fbc4dce51cbcd8c5a603d73
Stephan Chandler-Garcia <30474422+schandlergarcia@users.noreply.github.com>
2020-07-20 12:35:01 -0700

    Updated the project to handle the ExperienceBundle Deployment issues in Summer '20 (#215)
    * moved the experience bundle metadata to the mdapi deployment
    
    * removal of the coverage folder and added to gitignore
    
    * removal of debug statements
    
    * moved the profile to the mdapi deploy folder

--- force-app/main/default/lwc/orderItemTile/orderItemTile.js
+++ force-app/main/default/lwc/orderItemTile/orderItemTile.js
@@ -29,6 +29,7 @@ export default class OrderItemTile extends LightningElement {
     }
 
     /** Fires event to update the Order_Item__c SObject.  */
+
     saveOrderItem() {
         const event = new CustomEvent('orderitemchange', {
             detail: Object.assign({}, { Id: this.orderItem.Id }, this.form)
