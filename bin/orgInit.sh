2669b711fc41402051ca83b14d7af870f86a4242
pozil <philippe.ozil@gmail.com>
2020-09-17 23:07:54 +0200

    Updated sfdx-lwc-jest to 0.10.1

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -6,13 +6,14 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a ebikes -s -f config/project-scratch-def.json -d $DURATION
-sfdx force:source:push
-sfdx force:user:permset:assign -n ebikes
-sfdx force:data:tree:import --plan ./data/sample-data-plan.json
-echo "Sleeping 30s for Community deployment"
-sleep 30
-sfdx force:mdapi:deploy -u ebikes --deploydir mdapiDeploy/unpackaged -w 1
-sfdx force:community:publish -n E-Bikes
-sfdx force:org:open -p /lightning/page/home
+sfdx force:org:delete -p -u ebikes
+sfdx force:org:create -a ebikes -s -f config/project-scratch-def.json -d $DURATION && \
+sfdx force:source:push && \
+sfdx force:user:permset:assign -n ebikes && \
+sfdx force:data:tree:import --plan ./data/sample-data-plan.json && \
+echo "Sleeping 30s for Community deployment" && \
+sleep 30 && \
+sfdx force:mdapi:deploy -u ebikes --deploydir mdapiDeploy/unpackaged -w 5 && \
+sfdx force:community:publish -n E-Bikes && \
+sfdx force:org:open -p /lightning/page/home && \
 echo "Org is set up"
