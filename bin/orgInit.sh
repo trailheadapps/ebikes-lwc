16e570b629a66e28120cae4db9253cdb0f4c2dbd
Stephan Chandler-Garcia <30474422+schandlergarcia@users.noreply.github.com>
2020-10-09 09:08:53 -0700

    update to setup script to include walkthroughs

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -9,6 +9,7 @@ fi
 sfdx force:org:create -a ebikes -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n ebikes
+sfdx force:user:permset:assign -n Walkthroughs
 sfdx force:data:tree:import --plan ./data/sample-data-plan.json
 echo "Sleeping 30s for Community deployment"
 sleep 30
