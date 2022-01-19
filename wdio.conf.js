a26bb82e259c74b6adcff4d153ec01a6a62e0cee
pozil <philippe.ozil@gmail.com>
2022-01-19 19:30:43 +0100

    Config cleanup

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2021, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
-
 require('dotenv').config();
 
 const path = require('path');
--- wdioJasmineHelper.js
+++ wdioJasmineHelper.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2021, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
-
 const path = require('path');
 
 const DEFAULT_OPTIONS = {
@@ -12,7 +5,7 @@ const DEFAULT_OPTIONS = {
     root: __dirname,
     rootMode: 'root',
     ignore: [],
-    extensions: ['.js', '.ts']
+    extensions: ['.js']
 };
 
 require('@babel/register')(DEFAULT_OPTIONS);
