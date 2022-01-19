9d23c2a2f463e611d2924101dade6e58e7670d94
pozil <philippe.ozil@gmail.com>
2022-01-19 19:45:22 +0100

    Added temp utam-preview folder

---
+++ utam-preview/README.md
@@ -0,0 +1,4 @@
+# UTAM Preview
+
+Temporary package with Salesforce specific Page Objects.
+Exists only for the duration of UTAM pilot.
---
+++ utam-preview/index.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
+throw new Error(`
+PageObjects are not available directly from this entry point.
+Try: "utam-preview/pageObjects/[pageObjectName]"
+`);
