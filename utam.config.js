993fe8f41ed4f449fc6669356ff8edd9d623c86a
pozil <philippe.ozil@gmail.com>
2022-05-05 12:09:35 +0200

    Used native base PO path instead of alias

--- utam.config.js
+++ utam.config.js
@@ -3,9 +3,6 @@ module.exports = {
     pageObjectsFileMask: ['force-app/**/__utam__/**/*.utam.json'],
     // remap custom elements imports
     alias: {
-        'utam-sfdx/': 'ebikes-lwc/',
-        'utam-lightning/': 'salesforce-pageobjects/lightning/',
-        'utam-flexipage/': 'salesforce-pageobjects/flexipage/',
-        'utam-global/': 'salesforce-pageobjects/global/'
+        'utam-sfdx/': 'ebikes-lwc/'
     }
 };
