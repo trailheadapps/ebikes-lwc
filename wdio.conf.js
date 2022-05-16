e9ece142cd843d7530e8f4b1ad9605a65a4757a3
pozil <philippe.ozil@gmail.com>
2022-05-16 09:55:33 +0200

    Bumped dependencies

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
             'goog:chromeOptions': {
                 // to run chrome headless the following flags are required
                 // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-                // args: ['--headless', '--disable-gpu']
+                //args: ['--headless', '--disable-gpu', '--window-size=1920,1080']
             }
         }
     ],
