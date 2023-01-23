b3f565e2bb49ca2b9d0480b39add2aa4e1f7cbc7
pozil <pozil@salesforce.com>
2023-01-23 10:04:00 +0100

    wip: reseller orders

--- force-app/test/utam/reseller-orders.spec.js
+++ force-app/test/utam/reseller-orders.spec.js
@@ -6,11 +6,15 @@ import { logInSalesforce } from './utam-helper';
 import DesktopLayoutContainer from 'salesforce-pageobjects/navex/pageObjects/desktopLayoutContainer';
 import ObjectHome from 'salesforce-pageobjects/force/pageObjects/objectHome';
 import RecordActionWrapper from 'salesforce-pageobjects/global/pageObjects/recordActionWrapper';
+import RecordField from 'salesforce-pageobjects/record/flexipage/pageObjects/recordField';
 import RecordLayoutLookup from 'salesforce-pageobjects/records/pageObjects/recordLayoutLookup';
 import RecordHomeTemplateDesktop from 'salesforce-pageobjects/global/pageObjects/recordHomeTemplateDesktop';
+import FieldSection2 from 'salesforce-pageobjects/flexipage/pageObjects/fieldSection2';
+import Column2 from 'salesforce-pageobjects/flexipage/pageObjects/column2';
 import Tab2 from 'salesforce-pageobjects/flexipage/pageObjects/tab2';
 import ProductTileList from '../../../pageObjects/productTileList';
 import OrderBuilder from '../../../pageObjects/orderBuilder';
+import OrderCreateForm from '../../../pageObjects/orderCreateForm';
 
 const RECORD_PAGE_URL = /lightning\/r\/Order__c\/[a-z0-9]{18}\/view/i;
 
@@ -39,17 +43,58 @@ describe('ResellerOrders', () => {
         const listViewActionLink = await listViewActions.getActionLink('New');
         await listViewActionLink.click();
 
+        //await browser.debug();
+
         // Get create record modal
         const recActionWrapper = await utam.load(RecordActionWrapper);
-        const recForm = await recActionWrapper.getRecordForm();
-        await recForm.waitForLoad();
+        const recFormTemplate =
+            await recActionWrapper.waitForRecordHomeSingleColNoHeaderTemplateDesktop2();
+        let flexipageComponent = await recFormTemplate.getComponent2(
+            'flexipage_fieldSection'
+        );
+        /*
+        const createForm = await flexipageComponent.getContent(OrderCreateForm);
+        
+
+        console.log(JSON.stringify(createForm, null, 2));
+
+        await createForm.getAccountField();
+        
+        //const accountField = await createForm.getAccountField();
+        */
+
+        const section = await flexipageComponent.getContent(FieldSection2);
+        const columns = await section.getContent(Column2);
+
+        console.log(`
+        
+        ${columns.length}
+
+        `);
+
+        const recField = await columns[0].getFieldByID('RecordAccount_cField');
+        const recFieldContent = await recField.getRecordFieldContent(
+            RecordField
+        );
+        /*
+        //const recField = await genericComponent.getContent(RecordField);
+        const recLookupLayout = await recFieldContent.getFieldContentEditMode(
+            RecordLayoutLookup
+        );
+
+        //const createOrderModal = await recActionWrapper.getDetailsPanelContainer(RecordCreateModalForm);
+
+        //const recForm = await recActionWrapper.getRecordForm();
+        //await recForm.waitForLoad();
 
         // Select account
+        /*
         const recLayout = await recForm.getRecordLayout();
-        const recLayoutItem = await recLayout.getItem(1, 3, 1);
+        const recLayoutItem = await recLayout.getItem(1, 1, 1);
         const recLookupLayout = await recLayoutItem.getInputField(
             RecordLayoutLookup
         );
+
         const lookup = await recLookupLayout.getLookup();
         const lookupDesktop = await lookup.getLookupDesktop();
         const groupedCombobox = await lookupDesktop.getGroupedCombobox();
@@ -96,5 +141,6 @@ describe('ResellerOrders', () => {
 
         // Ensure that the items were added to the order
         await orderBuilder.waitForTotalItemsText('Total Items: 3');
+*/
     });
 });
